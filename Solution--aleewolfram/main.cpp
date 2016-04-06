25f0fc636dd53cf2465be4f4c90f01514978b1d5
aleewolfram <aleewolfram@gmail.com>
2016-04-05 17:58:21 -0700

    Create main.cpp

---
+++ Solution--aleewolfram/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include "test.h"
+#include "ponies.h"
+
+int main()
+{
+	int ignore;
+	testingexceptions(); // Exceptions & Templates
+	rungcd(); // Greatest Common Divisor 
+	runfib(); // Fibonacci Number
+	runpow(); // Power Function 
+	runtri(); // Triangular Number
+	std::cin >> ignore;
+
+}
